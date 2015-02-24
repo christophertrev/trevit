9443b6adf33c97c5f6af8379cc5c5a9c77345fab
Christopher Treviño <christophertrev@gmail.com>
2015-02-23 20:08:45 -0800

    (feat) add routes to magic

---
+++ client/js/index.js
@@ -0,0 +1,21 @@
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route
+
+
+// <Route handler={App}>
+//   <Route name="login" handler={Login}/>
+//   <Route name="logout" handler={Logout}/>
+//   <Route name="about" handler={About}/>
+//   <Route name="dashboard" handler={Dashboard}/>
+// </Route>
+
+
+// var routes = (
+//  <Route> </Route>
+// );
+
+
+// Router.run(routes, function (Handler) {
+//   React.render(<Handler/>, document.getElementById('example'));
+// });
