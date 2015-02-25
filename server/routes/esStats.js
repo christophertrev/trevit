3398620ae1705d099d824a2a72ad1e49fd72a363
Christopher Treviño <christophertrev@gmail.com>
2015-02-24 18:22:18 -0800

    (feat) add state and _stat route

--- server/routes/esStats.js
+++ server/routes/esStats.js
@@ -14,7 +14,29 @@ router.get('/', function(req, res){
  
 
 
+router.get('/_stats', function(req, res){
+  client.cluster.stats(function (err, resp) {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.send(resp);
+    }
+  });
+});
 
+
+// state
+
+
+router.get('/_state', function(req, res){
+  client.cluster.state(function (err, resp) {
+    if (err) {
+      res.send(err.message);
+    } else {
+      res.send(resp);
+    }
+  });
+});
 
 
 module.exports = router; 
